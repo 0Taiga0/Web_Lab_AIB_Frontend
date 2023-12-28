65bea9249729f4b7639f421d337a2760eb8f9f03
Egor_Keen <Egor.KruchininX@yandex.ru>
2023-12-28 13:17:19 +0300

    added lab13

--- labs/Lab_12_python_intro/Solutions/task_2/task2.py
+++ labs/Lab_12_python_intro/Solutions/task_2/task2.py
@@ -1,10 +1,18 @@
-
-def sum_median(arr):
-    count = 0
-    for item in arr:
-        tarr = sorted(arr[:item])
-        count += tarr[len(tarr) // 2] if len(tarr) % 2 != 0 else tarr[len(tarr) // 2 - 1]
-    return count
+from heapq import heappop, heappush
 
 
-print(sum_median([5, 10, 8, 1, 7, 3, 9, 6, 2, 4]))
+def medians_sum(numbers: list) -> int:
+    mediansSum = 0
+    firstHalf, secondHalf = [], []
+    for number in numbers:
+        heappush(firstHalf, -number)
+        heappush(secondHalf, -heappop(firstHalf))
+        if len(firstHalf) < len(secondHalf):
+            heappush(firstHalf, -heappop(secondHalf))
+        mediansSum += -firstHalf[0]
+    return mediansSum
+
+
+if __name__ == "__main__":
+    arr = [5, 10, 8, 1, 7, 3, 9, 6, 2, 4]
+    print(medians_sum(arr))
